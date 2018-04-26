f99cc388c8dfa04e1dad61f27b0e53a149b5a68d
NighTurs <nighturs@gmail.com>
2018-04-26 21:39:03 +0300

    Build own barracks if enemy rushes

--- src/main/java/com/github/nighturs/codingame/coderoyale/Player.java
+++ src/main/java/com/github/nighturs/codingame/coderoyale/Player.java
@@ -587,7 +587,7 @@ class Player {
                     return Optional.of(new BuildingDecision(StructureType.BARRACKS,
                             BarracksType.KNIGHT,
                             (maxDistToEnemyQueen - distToEnemyQueen) / 2 + enemyKnightsBonus));
-                } else if (myBarracksCount == 0 && myMinesCount >= 2) {
+                } else if (myBarracksCount == 0 && (myMinesCount >= 2 || enemyBarracksCount != 0)) {
                     return Optional.of(new BuildingDecision(StructureType.BARRACKS,
                             BarracksType.KNIGHT,
                             (maxDistToEnemyQueen - distToEnemyQueen) / 2 + enemyKnightsBonus));
