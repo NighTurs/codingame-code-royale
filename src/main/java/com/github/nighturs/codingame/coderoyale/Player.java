71783687f0da51d5d431040c51ec19ac4d08197e
NighTurs <nighturs@gmail.com>
2018-04-28 17:23:52 +0300

    Fix spare resources

--- src/main/java/com/github/nighturs/codingame/coderoyale/Player.java
+++ src/main/java/com/github/nighturs/codingame/coderoyale/Player.java
@@ -831,8 +831,8 @@ class Player {
                             x.getY(),
                             gameState.getEnemyQueen().getX(),
                             gameState.getEnemyQueen().getY())));
-            if (knightBarrack.isPresent() && (waitTillNext <= 0 || waitTillNext > WAITING_THRESHOLD) || (myKnightsAreOut
-                    && gold >= KNIGHT_COST)) {
+            if (knightBarrack.isPresent() && ((waitTillNext <= 0 || waitTillNext > WAITING_THRESHOLD) || (myKnightsAreOut
+                    && gold >= KNIGHT_COST))) {
                 gold -= KNIGHT_COST;
                 trainSites.add(knightBarrack.get().getId());
             }
